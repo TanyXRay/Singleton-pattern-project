8a997426ca1150d6b382beffa1a30f4b49c24372
Tatyana Chernyadieva <tany.taray@gmail.com>
2022-11-09 21:13:21 +0400

    fix files

--- src/main/java/filter/Filter.java
+++ src/main/java/filter/Filter.java
@@ -12,6 +12,11 @@ public class Filter {
         this.threshold = threshold;
     }
 
+    /**
+     * Метод производит фильтрацию списка с логированием действий
+     * @param source список с рандомными числами
+     * @return список отфильтрованный по критерию
+     */
     public List<Integer> filterOut(List<Integer> source) {
         Logger log = Logger.getInstance();
         List<Integer> resultList = new ArrayList<>();
